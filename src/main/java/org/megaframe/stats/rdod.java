fee3fa7237e55e724fdbf55b31017d587a0a6299
Saul Rosa <rosa.saul@megaframe.org>
2015-03-02 15:51:00 -0800

    More of the cli built, and some package wrapping stuff

--- src/main/java/org/megaframe/stats/rdod.java
+++ src/main/java/org/megaframe/stats/rdod.java
@@ -1,14 +1,20 @@
 package org.megaframe.stats;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import static org.kohsuke.args4j.ExampleMode.ALL;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.spi.BooleanOptionHandler;
 
-import org.apache.commons.cli.BasicParser;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Hashtable;
 
 /**
  * Reads in a CSV and Adds in a column "Category" with the
@@ -19,37 +25,136 @@ import org.apache.commons.cli.ParseException;
  * @version 1
  */
 public class rdod {
-  // Setup Logger for CLI
-  private static final Logger log = Logger.getLogger(rdod.class.getName());
 
-  private static String input = null;
-  //Output File
-  private static String output = null;
+  // Setup CLI Options
+  @Option(name="-o",usage="output to this file",metaVar="OUTPUT")
+  private String output;
 
-  public static ArrayList list = null;
+  @Option(name="-match",usage="Regex string match for columns to use",metaVar="REGEX")
+  private String match = "";
 
-  public static double rsigma = 0;
+  @Option(name="-rsigma",usage="Radial Sigma to limit by, default 3",metaVar="SIGMA")
+  private double rsigma = 3;
 
-  public static int minNeighbors = 0;
+  @Option(name="-neighbors",usage="Minimum number of neighbors, default 6",metaVar="NEIGHBORS")
+  private int neighbors = 6;
 
-  public static int ksets = 0;
+  @Option(name="-rdist",usage="Radial distance to limit by, overides -rsigma, no default",metaVar="DISTANCE")
+  private double rdist = 0;
 
-  public static void main(String[] args){
-    // Parse Args
-    rdod rdod = new rdod();
-    rdod.parseArgs(args);
-    rdod.processFile(input);
+  @Option(name="-ksets",usage="Number of K-Means groups to form, no default",metaVar="KSETS")
+  private int ksets = 0;
 
-    RadialDensityOutlier engine = new RadialDensityOutlier();
-    double[] categories = engine.categorize(list,rsigma,minNeighbors,ksets);
-    dumpOutput(output,categories,list);
+  // receives other command line parameters than options
+  @Argument
+  private List<String> inputFiles = new ArrayList<String>();
+
+  public static void main(String[] args) throws IOException {
+    new rdod().doMain(args);
   }
 
-  private void parseArgs(String[] args) {
-    Options options = new Options();
-    options.addOption("h", "help", false, "show help.");
+  public void doMain(String[] args) throws IOException {
+    CmdLineParser parser = new CmdLineParser(this);
+    try {
+      parser.parseArgument(args);
+      if( inputFiles.isEmpty() ){
+        throw new CmdLineException(parser,"No arguments is given"); }
+      if( ksets == 0 ){
+        throw new CmdLineException(parser,"ksets must be set to an integer greater than 0"); }
+
+    } catch( CmdLineException e ) {
+      System.err.println(e.getMessage());
+      System.err.println("rdod [options...] files...");
+      parser.printUsage(System.err);
+      System.err.println("\n  Example: rdod"+parser.printExample(ALL)+" inputFile");
+      return;
+    }
+
+    PrintStream ps = null;
+    if(output != null){
+      ps = new PrintStream(output);
+      System.setOut(ps);
+    }
+
+    csvReader csv = new csvReader();
+    ArrayList data = null;
+    try {
+      data = csv.parseCSV(inputFiles.get(0),match);
+    } catch(IOException e) {
+      System.err.format("Exception occurred trying to read '%s'.", inputFiles.get(0));
+      System.err.println("Error : " + e.getMessage());
+      if(output != null){ ps.close(); }
+      return;
+    }
+
+    RadialDensityOutlier rdod = new RadialDensityOutlier();
+    int[] categories = rdod.categorize(data,rsigma,neighbors,ksets);
+
+    // Print the returned categories
+    System.out.println("Category," + csv.getHeader());
+    for(int row = 0; row < categories.length; row++){
+      System.out.println(categories[row]+","+csv.getLine(row));
+    }
+
+    if(output != null){ ps.close(); }
   }
 
+  private ArrayList readFile(String input){
+    ArrayList data = new ArrayList();
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(input));
+      String line = reader.readLine();
+      while ((line = reader.readLine()) != null){
+        //records.add(line);
+      }
+      reader.close();
+    } catch(Exception e) {
+      System.err.format("Exception occurred trying to read '%s'.", input);
+      System.err.println("Error : " + e.getMessage());
+      System.exit(0);
+    }
+    return data;
+  }
+}
+
+/** CSV File parser for rdod
+ * Reads the input CSV grabs the required columns
+ * Keeps the original Lines for dumping back content
+ */
+class csvReader {
+  // Original Lines Coming in
+  private ArrayList<String> origLines = new ArrayList<String>();
+  private String lineHeader = null;
+  private Hashtable<String, Integer> header = new Hashtable<String, Integer>();
+  private ArrayList<String> matchedCols = new ArrayList<String>();
+
+  public ArrayList parseCSV(String input, String matcher) throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(input));
+    ArrayList data = new ArrayList();
+    lineHeader = reader.readLine();
+    String[] headerItems = lineHeader.split(",");
+    for(int i = 0; i < headerItems.length; i++){
+      header.put(headerItems[i],i);
+      if(headerItems[i].matches(matcher)){ matchedCols.add(headerItems[i]); }
+    }
+
+    String line;
+    while ((line = reader.readLine()) != null){
+      double[] vector = new double[matchedCols.size()];
+      String[] items = line.split(",");
+      int i = 0;
+      for(String col : matchedCols){
+        vector[i] = Double.parseDouble( items[ header.get(col) ] );
+        data.add(vector);
+        i++;
+      }
+    }
+    return data;
+  }
+
+  public String getHeader(){ return this.lineHeader; }
+
+  public String getLine(int row){ return this.origLines.get(row); }
 }
 
 
